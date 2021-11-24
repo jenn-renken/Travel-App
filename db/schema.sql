e93a023268c8fc9ba03d508ba039b16fd73b9d5b
jenn-renken <88343948+jenn-renken@users.noreply.github.com>
2021-11-23 21:00:12 -0500

    added template code

---
+++ db/schema.sql
@@ -0,0 +1,2 @@
+DROP DATABASE IF EXISTS travel_app_db;
+CREATE DATABASE travel_app_db;
---
+++ models/user.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create user model
+class User extends Model {
+    checkPassword(loginPw) {
+        return (loginPw, this.password);
+    }
+}
+
+// define table columns and configuration
+User.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+        validate: {
+          isEmail: true
+        }
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+          len: [4]
+        }
+      }
+    },
+    {
+      sequelize,
+      timestamps: false,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'user'
+    }
+  );
+
+
+module.exports = User;
