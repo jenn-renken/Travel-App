7c80f1a93d8c272875ce9190b50754989c27e981
Rahil Rahimi <rahilrahimi1362@gmail.com>
2021-12-02 20:58:12 -0500

    login/signup logic

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -70,13 +70,22 @@ router.get('/post/:id', (req, res) => {
     });
 });
 
-// router.get('/login', (req, res) => {
-//   if (req.session.loggedIn) {
-//     res.redirect('/');
-//     return;
-//   }
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
 
-//   res.render('login');
-// });
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
 
 module.exports = router;
--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,32 +2,28 @@ async function signupFormHandler(event) {
     event.preventDefault();
   
     // const username = document.querySelector('#username-signup').value.trim();
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
+    const usernameEl = document.querySelector('#username-signup');
+    const emailEl = document.querySelector('#email-signup');
+    const passwordEl = document.querySelector('#password-signup');
   
     if (username && email && password) {
-      const response = await fetch('/api/users', {
+      await fetch('/api/users', {
         method: 'post',
         body: JSON.stringify({
-          username,
-          email,
-          password
+          username: usernameEl.value,
+          email: emailEl.value,
+          password: passwordEl.value
         }),
         headers: { 'Content-Type': 'application/json' }
-      });
-  
-      // check the response status
-      if (response.ok) {
-        document.location.replace('/login');
-        console.log('success');
-      } else {
-        alert(response.statusText);
-      }
+      })
+      .then(() => {
+        document.location.replace('/')
+      })
+      .catch(err => console.log(err));
     }
   }
   
-document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+document.querySelector('#signup-form').addEventListener('submit', signupFormHandler);
 
 async function loginFormHandler(event) {
     event.preventDefault();
@@ -36,21 +32,19 @@ async function loginFormHandler(event) {
     const passwordEl = document.querySelector('#password-login');
   
     if (email && password) {
-      const response = await fetch('/api/users/login', {
+      await fetch('/api/users/login', {
         method: 'post',
         body: JSON.stringify({
           email: emailEl.value,
           password: passwordEl.value
         }),
         headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/login');
-      } else {
-        alert(response.statusText);
-      }
+      })
+      .then(() => {
+        document.location.replace('/')
+      })
+      .catch(err => console.log(err));
     }
   }
   
-  document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+  document.querySelector('#login-form').addEventListener('submit', loginFormHandler);
